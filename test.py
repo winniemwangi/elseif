eea2eb6b556023e7e4b7f86065326d71d17436c0
winniemwangi <winniewameremwangi@gmail.com>
2022-04-23 19:33:01 +0300

    add elseif statement

---
+++ test.py
@@ -0,0 +1,11 @@
+
+for i in range(1,51):
+    if i%3==0 and i%5==0:
+        print("Fizzbuzz")
+    elif(i%3==0):
+        print("fizz")
+    elif(i%5==0):
+        print("Buzz")
+    else:
+        print(i)
+       
